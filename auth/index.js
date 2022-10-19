2277d6c054fa24e817f5d15af76a27b03be0831c
AztFireSpell <alondigu@gmail.com>
2022-10-18 21:18:37 -0500

    Add Token Bearer

--- auth/index.js
+++ auth/index.js
@@ -1,9 +1,53 @@
 const jwt = require('jsonwebtoken');
+const config = require('../config');
+const secret = config.jwt.secret;
 
 function sign(data){
-    return jwt.sign(data, 'secreto');
+    return jwt.sign(data, secret);
+}
+
+function verify(token){
+    return jwt.verify(token, secret)
+}
+
+const check = {
+    own: function(req, owner){
+        const tokendecode= decodeHeader(req);
+        console.log(tokendecode);
+
+        //comprobar si es o no propio el perfil
+        if(tokendecode.id !== owner){
+            throw new Error('No puedes editar este elemento');
+        }
+    },
+}
+
+function getToken(auth){
+    //Bearer suadsdj817y3rh2832
+    if(!auth){
+        throw new Error('No viene el token');
+    }
+
+    if(auth.indexOf('Bearer ') === -1){
+        throw new Error('Formato Invalido');
+    }
+
+    let token = auth.replace('Bearer ', '');
+
+    return token;
+}
+
+function decodeHeader(req){
+    const authorization = req.headers.authorization || '';
+    const token = getToken(authorization);
+    const decoded = verify(token);
+
+    req.user = decoded;
+
+    return decoded;
 }
 
 module.exports = {
     sign,
+    check,
 };
